5061fe421eecdcd4bfbbb735402a9e8ad7d13403
Carolina Pinheiro dos Santos <54641272832@>
2025-04-04 16:39:36 -0300

    terceira

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('api/servicos/', views.criar_servico),
     path('api/servicos/<int:id>/', views.detalhe_servico),
     path('api/agendamentos/', views.listar_agendamento),
-    path('api/servicos/', views.criar_agendamento),
+    path('api/agendamentos/', views.criar_agendamento),
     path('api/agendamentos/<int:id>/', views.detalhe_agendamento)
 ]
