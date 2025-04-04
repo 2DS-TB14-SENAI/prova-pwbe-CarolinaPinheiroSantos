84a76cf6493fc8f9d51ed9119eeeadb20fe88e8a
Carolina Pinheiro dos Santos <54641272832@>
2025-04-04 16:36:28 -0300

    segunda

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/servicos/', views.listar_criar_servico),
+    path('api/servicos/', views.listar_servico),
+    path('api/servicos/', views.criar_servico),
     path('api/servicos/<int:id>/', views.detalhe_servico),
-    path('api/agendamentos/', views.listar_criar_agendamento),
+    path('api/agendamentos/', views.listar_agendamento),
+    path('api/servicos/', views.criar_agendamento),
     path('api/agendamentos/<int:id>/', views.detalhe_agendamento)
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -6,23 +6,26 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def listar_criar_servico(request):
+@api_view(['GET'])
+def listar_servico(request):
     if request.method == 'GET':
         servico = Servico.objects.all()
         serializer = ServicoSerializer(servico, many=True)
         return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
-    elif request.method == 'POST':
+@api_view(['POST'])    
+def criar_servico(request):
+    if request.method == 'POST':
         serializer = ServicoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
-    else:
-        return Response({'Erro': 'Escolha requisição GET ou PUT'},status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET'])
 def detalhe_servico(request, id):
@@ -36,23 +39,26 @@ def detalhe_servico(request, id):
 
 
 
-@api_view(['GET', 'POST'])
-def listar_criar_agendamento(request):
+@api_view(['GET'])
+def listar_agendamento(request):
     if request.method == 'GET':
         agendamento = Agendamento.objects.all()
         serializer = AgendamentoSerializer(agendamento, many=True)
         return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
-    elif request.method == 'POST':
+@api_view(['POST'])    
+def criar_agendamento(request):
+    if request.method == 'POST':
         serializer = AgendamentoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-            
-    else:
-        return Response({'Erro': 'Escolha requisição GET ou PUT'},status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     
 @api_view(['GET'])
 def detalhe_agendamento(request, id):
